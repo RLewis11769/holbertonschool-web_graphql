f70a3d852b382dfec29e1196d46ecd5ddc9eece6
Rachel Lewis <RLewis11769@gmail.com>
2022-01-26 19:29:17 -0600

    Add info for project query/schema

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -2,7 +2,8 @@ const {
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLID,
 } = require('graphql');
 const lodash = require('lodash');
 
@@ -12,13 +13,28 @@ const tasks = [
   id: '1',
   title: 'Create your first webpage',
   weight: 1,
-  description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'
+  description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
+  projectId: 1,
   },
   {
     id: '2',
     title: 'Structure your webpage',
     weight: 1,
-    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'
+    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order',
+    projectId: 1,
+  }
+]
+
+const projects = [
+  {id: '1',
+    title: 'Advanced HTML',
+    weight: 1,
+    description: 'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!'
+  },
+  {id: '2',
+    title: 'Bootstrap',
+    weight: 1,
+    description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.'
   }
 ]
 
@@ -33,6 +49,16 @@ const TaskType = new GraphQLObjectType({
   })
 });
 
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: GraphQLID },
+    title: { type: GraphQLString },
+    weight: { type: GraphQLInt },
+    description: { type: GraphQLString },
+  })
+});
+
 // Root query for defining types of queries
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -41,7 +67,12 @@ const RootQuery = new GraphQLObjectType({
       type: TaskType,
       args: { id: { type: GraphQLString } },
       resolve: (parent, args) => lodash.find(tasks, { id: args.id })
-    }
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLString } },
+      resolve: (parent, args) => lodash.find(projects, { id: args.id })
+    },
   })
 });
 
