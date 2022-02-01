a1dd793e47c0c238e11cb0c7d9d93cbc053883e6
Rachel Lewis <RLewis11769@gmail.com>
2022-01-31 22:00:07 -0600

    Task query should accept arg of type id

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -10,36 +10,11 @@ const {
 const Project = require('../models/project');
 const Task = require('../models/task');
 
-// Root query for defining types of queries
-const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
-  fields: () => ({
-    task: {
-      type: TaskType,
-      args: { id: { type: GraphQLString } },
-      resolve: (parent, args) => Task.findById(args.id)
-    },
-    project: {
-      type: ProjectType,
-      args: { id: { type: GraphQLID } },
-      resolve: (parent, args) => Project.findById(args.id)
-    },
-    tasks: {
-      type: new GraphQLList(TaskType),
-      resolve: () => Task.find({})
-    },
-    projects: {
-      type: new GraphQLList(ProjectType),
-      resolve: () => Project.find({})
-    }
-  })
-});
-
 // Schema definition for tasks
 const TaskType = new GraphQLObjectType({
   name: 'Task',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     title: { type: GraphQLString },
     weight: { type: GraphQLInt },
     description: { type: GraphQLString },
@@ -62,6 +37,31 @@ const ProjectType = new GraphQLObjectType({
       type: new GraphQLList(TaskType),
       resolve: (parent, args) => Task.find({ projectId: parent.id })
     },
+  })
+});
+
+// Root query for defining types of queries
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: () => ({
+    task: {
+      type: TaskType,
+      args: { id: { type: GraphQLID } },
+      resolve: (parent, args) => Task.findById(args.id)
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve: (parent, args) => Project.findById(args.id)
+    },
+    tasks: {
+      type: new GraphQLList(TaskType),
+      resolve: () => Task.find({})
+    },
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve: () => Project.find({})
+    }
   })
 });
 
